c130b70a68a2d0e2b4c9cfdd20e5be5a2fcc08ff
jonesrmj <51058123+jonesrmj@users.noreply.github.com>
2021-08-06 16:00:11 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ryan Jones on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
